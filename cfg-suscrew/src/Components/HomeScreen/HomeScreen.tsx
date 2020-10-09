bce9694ee8d2af2a74ffebfeec1eff09d7bb4ea1
fatraphael95 <raphaelchen1995@gmail.com>
2020-10-09 11:27:55 +0100

    Added LoginPage (Not Done)

--- cfg-suscrew/src/Components/HomeScreen/HomeScreen.tsx
+++ cfg-suscrew/src/Components/HomeScreen/HomeScreen.tsx
@@ -1,12 +1,38 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { StyleSheet, View, Text, TextInput, StatusBar } from 'react-native';
 
 const HomeScreen = () => {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home!</Text>
+    <View style={styles.container}>
+      <StatusBar backgroundColor="blue" barStyle="light-content" />
+      <Text style={{color: "#000", fontSize: 80}}>Welcome</Text>
+      <Text style={{color: "#96A7AF", fontSize: 30}}>Sign in to continue</Text>
+      <TextInput style={styles.inputBox} placeholder="Username"/>
+      <TextInput style={styles.inputBox} placeholder="Password"/>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#fff",
+    paddingLeft: 60, 
+    paddingRight: 6,
+  },
+
+  inputBox: {
+    width: 300,
+    underlineColorAndroid: "rgba(0,0,0,0)",
+    backgroundColor: "rgba(150, 167, 175, 0.3)",
+    borderRadius: 25,
+    paddingHorizontal: 50,
+    placeholderTextColor: "#fff",
+    fontSize: 16,
+    marginVertical: 5,
+  }
+})
 
 export default HomeScreen
