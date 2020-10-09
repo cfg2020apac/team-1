02313c149f34e6871c2f81ed2bcb532c8b0d65f1
yanbutan <ynaung6995@gmail.com>
2020-10-09 22:59:57 +0800

    removed failing css

--- cfg-suscrew/src/Components/HomeScreen/HomeScreen.tsx
+++ cfg-suscrew/src/Components/HomeScreen/HomeScreen.tsx
@@ -2,37 +2,39 @@ import React from 'react';
 import { StyleSheet, View, Text, TextInput, StatusBar } from 'react-native';
 
 const HomeScreen = () => {
-  return (
-    <View style={styles.container}>
-      <StatusBar backgroundColor="blue" barStyle="light-content" />
-      <Text style={{color: "#000", fontSize: 80}}>Welcome</Text>
-      <Text style={{color: "#96A7AF", fontSize: 30}}>Sign in to continue</Text>
-      <TextInput style={styles.inputBox} placeholder="Username"/>
-      <TextInput style={styles.inputBox} placeholder="Password"/>
-    </View>
-  );
-}
+    return (
+        <View style={styles.container}>
+            <StatusBar backgroundColor="blue" barStyle="light-content" />
+            <Text style={{ color: '#000', fontSize: 80 }}>Welcome</Text>
+            <Text style={{ color: '#96A7AF', fontSize: 30 }}>
+                Sign in to continue
+            </Text>
+            <TextInput style={styles.inputBox} placeholder="Username" />
+            <TextInput style={styles.inputBox} placeholder="Password" />
+        </View>
+    );
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "#fff",
-    paddingLeft: 60, 
-    paddingRight: 6,
-  },
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#fff',
+        paddingLeft: 60,
+        paddingRight: 6
+    },
 
-  inputBox: {
-    width: 300,
-    underlineColorAndroid: "rgba(0,0,0,0)",
-    backgroundColor: "rgba(150, 167, 175, 0.3)",
-    borderRadius: 25,
-    paddingHorizontal: 50,
-    placeholderTextColor: "#fff",
-    fontSize: 16,
-    marginVertical: 5,
-  }
-})
+    inputBox: {
+        width: 300,
+        // underlineColorAndroid: "rgba(0,0,0,0)",
+        backgroundColor: 'rgba(150, 167, 175, 0.3)',
+        borderRadius: 25,
+        paddingHorizontal: 50,
+        // placeholderTextColor: "#fff",
+        fontSize: 16,
+        marginVertical: 5
+    }
+});
 
-export default HomeScreen
+export default HomeScreen;
